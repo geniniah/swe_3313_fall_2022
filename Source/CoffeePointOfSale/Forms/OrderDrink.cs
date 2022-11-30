9f38f4b4a0d0a4592077f850958d5225bad30319
honoreg <fantagelife245@gmail.com>
2022-11-29 22:55:28 -0500

    Added a OrderDrink Form

---
+++ Source/CoffeePointOfSale/Forms/OrderDrink.cs
@@ -0,0 +1,32 @@
+﻿using CoffeePointOfSale.Configuration;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoffeePointOfSale.Forms
+{
+    public partial class OrderDrink : Form
+    {
+        private IAppSettings? _appSettings;
+        private decimal _taxRate;
+        private decimal _rewardPerDollar;
+        public OrderDrink()
+        {
+            InitializeComponent();
+        }
+        public OrderDrink(IAppSettings appSettings) : this()
+        {
+            _appSettings = appSettings;
+            _rewardPerDollar = appSettings.Rewards.PointsPerDollar;
+            _taxRate = appSettings.Tax.Rate;
+
+        }
+
+    }
+}
