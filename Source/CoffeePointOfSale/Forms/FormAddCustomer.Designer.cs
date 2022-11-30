4ae38fb04c9693063c267ae34c6ef6e14adc2fb9
Joshua Poore <joshua.poore@gmail.com>
2022-11-30 06:15:07 -0500

    Customer Phone Unique almost working perfectly

--- Source/CoffeePointOfSale/Forms/FormAddCustomer.Designer.cs
+++ Source/CoffeePointOfSale/Forms/FormAddCustomer.Designer.cs
@@ -29,11 +29,19 @@
         private void InitializeComponent()
         {
             this.btnClose = new System.Windows.Forms.Button();
+            this.txtBoxFirstName = new System.Windows.Forms.TextBox();
+            this.txtBoxLastName = new System.Windows.Forms.TextBox();
+            this.txtBoxPhoneNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labError = new System.Windows.Forms.Label();
+            this.btnCreateCustomer = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnClose
             // 
-            this.btnClose.Location = new System.Drawing.Point(234, 43);
+            this.btnClose.Location = new System.Drawing.Point(1218, 651);
             this.btnClose.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(117, 37);
@@ -42,20 +50,103 @@
             this.btnClose.UseVisualStyleBackColor = true;
             this.btnClose.Click += new System.EventHandler(this.OnClickBtnClose);
             // 
+            // txtBoxFirstName
+            // 
+            this.txtBoxFirstName.Location = new System.Drawing.Point(591, 179);
+            this.txtBoxFirstName.Name = "txtBoxFirstName";
+            this.txtBoxFirstName.Size = new System.Drawing.Size(125, 27);
+            this.txtBoxFirstName.TabIndex = 1;
+            // 
+            // txtBoxLastName
+            // 
+            this.txtBoxLastName.Location = new System.Drawing.Point(591, 256);
+            this.txtBoxLastName.Name = "txtBoxLastName";
+            this.txtBoxLastName.Size = new System.Drawing.Size(125, 27);
+            this.txtBoxLastName.TabIndex = 2;
+            // 
+            // txtBoxPhoneNumber
+            // 
+            this.txtBoxPhoneNumber.Location = new System.Drawing.Point(591, 331);
+            this.txtBoxPhoneNumber.Name = "txtBoxPhoneNumber";
+            this.txtBoxPhoneNumber.Size = new System.Drawing.Size(125, 27);
+            this.txtBoxPhoneNumber.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(613, 143);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(614, 222);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Last Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(591, 299);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Phone Number";
+            // 
+            // labError
+            // 
+            this.labError.AutoSize = true;
+            this.labError.Location = new System.Drawing.Point(627, 389);
+            this.labError.Name = "labError";
+            this.labError.Size = new System.Drawing.Size(0, 20);
+            this.labError.TabIndex = 7;
+            // 
+            // btnCreateCustomer
+            // 
+            this.btnCreateCustomer.Location = new System.Drawing.Point(591, 430);
+            this.btnCreateCustomer.Name = "btnCreateCustomer";
+            this.btnCreateCustomer.Size = new System.Drawing.Size(136, 42);
+            this.btnCreateCustomer.TabIndex = 8;
+            this.btnCreateCustomer.Text = "Create Customer";
+            this.btnCreateCustomer.UseVisualStyleBackColor = true;
+            this.btnCreateCustomer.Click += new System.EventHandler(this.btnCreateCustomer_Click);
+            // 
             // FormAddCustomer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1348, 721);
+            this.Controls.Add(this.btnCreateCustomer);
+            this.Controls.Add(this.labError);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBoxPhoneNumber);
+            this.Controls.Add(this.txtBoxLastName);
+            this.Controls.Add(this.txtBoxFirstName);
             this.Controls.Add(this.btnClose);
             this.Margin = new System.Windows.Forms.Padding(3, 9, 3, 9);
             this.Name = "FormAddCustomer";
             this.Text = "FormAddCustomer";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private Button btnClose;
+        private TextBox txtBoxFirstName;
+        private TextBox txtBoxLastName;
+        private TextBox txtBoxPhoneNumber;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label labError;
+        private Button btnCreateCustomer;
     }
 }
--- Source/CoffeePointOfSale/Forms/FormAddCustomer.cs
+++ Source/CoffeePointOfSale/Forms/FormAddCustomer.cs
@@ -1,4 +1,5 @@
 ﻿using CoffeePointOfSale.Configuration;
+using CoffeePointOfSale.Services.Customer;
 using CoffeePointOfSale.Services.FormFactory;
 using System;
 using System.Collections.Generic;
@@ -15,9 +16,12 @@ namespace CoffeePointOfSale.Forms
     public partial class FormAddCustomer : Base.FormNoCloseBase
     {
         private IAppSettings _appSettings;
-        public FormAddCustomer(IAppSettings appSettings) : this()
+        private ICustomerService _customerService;
+
+        public FormAddCustomer(IAppSettings appSettings, ICustomerService customerService) : this()
         {
             _appSettings = appSettings;
+            _customerService = customerService;
         }
 
         public FormAddCustomer()
@@ -34,6 +38,56 @@ namespace CoffeePointOfSale.Forms
         {
             Close(); //closes this form
             FormFactory.Get<FormMain>().Show(); //re-opens the main form
+        }
+        // This is used to make sure the phone number is unique
+        static private bool Unique = true;
+        private void btnCreateCustomer_Click(object sender, EventArgs e)
+        {
+            Unique = true;
+            if((txtBoxFirstName.Text == string.Empty)||(txtBoxLastName.Text == string.Empty))
+            {
+                labError.Text = "Please fill out the customer's name.";
+            }
+            else if(txtBoxPhoneNumber.Text== string.Empty)
+            {
+                labError.Text = "Please type in a phone number.";
+            }
+            else
+            {
+                if(txtBoxPhoneNumber.Text.Length == 10)
+                {
+                    if(int.TryParse(txtBoxPhoneNumber.Text, out int value)){
+                        var customerList = _customerService.Customers.List;
+                        foreach (var customer in customerList)
+                        {
+                            if(customer.Phone == txtBoxPhoneNumber.Text)
+                            {
+                                Unique = false;
+                            }
+                        }
+                        if (Unique)
+                        {
+                            // Put into Json here
+                            labError.Text = "All inputs are good.";
+                            Close(); //closes this form
+                            FormFactory.Get<OrderDrink>().Show(); //re-opens the main form
+                        }
+                        else
+                        {
+                            labError.Text = "That phone number already exists as a customer.";
+                        }
+                    }
+                    else
+                    {
+                        // There is an issue here with the TryParse not being able to tell anything with 404 in front of it apart.
+                        labError.Text = "Please do not include anything that is not a number in the phone number";
+                    }
+                }
+                else
+                {
+                    labError.Text = "Please enter a 10 digit phone number, no dashes.";
+                }
+            }
         }
     }
 }
