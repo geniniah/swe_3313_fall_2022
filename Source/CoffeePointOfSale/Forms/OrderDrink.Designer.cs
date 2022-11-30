70af4eb848b0fd810688ee55cabbffc56c16483d
honoreg <fantagelife245@gmail.com>
2022-11-30 02:26:06 -0500

    Changes To Order Drink Screen 2

--- Source/CoffeePointOfSale/Forms/OrderDrink.Designer.cs
+++ Source/CoffeePointOfSale/Forms/OrderDrink.Designer.cs
@@ -28,59 +28,299 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.btnLatte = new System.Windows.Forms.Button();
+            this.btnIcedLatte = new System.Windows.Forms.Button();
+            this.btnGreenLatte = new System.Windows.Forms.Button();
+            this.btnAddToOrder = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCoffee = new System.Windows.Forms.Button();
+            this.btnIcedWater = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnMediumCupSize = new System.Windows.Forms.Button();
+            this.btnLargeCupSize = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.LabelSizeOfDrinkLeft = new System.Windows.Forms.Label();
+            this.LabelCustomizationsLeft = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnIncreaseQuantity = new System.Windows.Forms.Button();
+            this.btnDecreaseQuantity = new System.Windows.Forms.Button();
+            this.labelQuantityNumber = new System.Windows.Forms.Label();
+            this.btnPaymentOrderDrinkScreen = new System.Windows.Forms.Button();
+            this.btnCancelOrderDrinkScreen = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // btnLatte
             // 
-            this.button1.Location = new System.Drawing.Point(34, 28);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(112, 65);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnLatte.Location = new System.Drawing.Point(26, 56);
+            this.btnLatte.Name = "btnLatte";
+            this.btnLatte.Size = new System.Drawing.Size(112, 65);
+            this.btnLatte.TabIndex = 0;
+            this.btnLatte.Text = "Select";
+            this.btnLatte.UseVisualStyleBackColor = true;
+            
             // 
-            // button2
+            // btnIcedLatte
             // 
-            this.button2.Location = new System.Drawing.Point(174, 28);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(112, 65);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnIcedLatte.Location = new System.Drawing.Point(169, 56);
+            this.btnIcedLatte.Name = "btnIcedLatte";
+            this.btnIcedLatte.Size = new System.Drawing.Size(112, 65);
+            this.btnIcedLatte.TabIndex = 1;
+            this.btnIcedLatte.Text = "Select";
+            this.btnIcedLatte.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // btnGreenLatte
             // 
-            this.button3.Location = new System.Drawing.Point(314, 28);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(112, 65);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnGreenLatte.Location = new System.Drawing.Point(341, 112);
+            this.btnGreenLatte.Name = "btnGreenLatte";
+            this.btnGreenLatte.Size = new System.Drawing.Size(112, 65);
+            this.btnGreenLatte.TabIndex = 2;
+            this.btnGreenLatte.Text = "Select";
+            this.btnGreenLatte.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // btnAddToOrder
             // 
-            this.button4.Location = new System.Drawing.Point(589, 568);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(135, 117);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btnAddToOrder.Location = new System.Drawing.Point(34, 556);
+            this.btnAddToOrder.Name = "btnAddToOrder";
+            this.btnAddToOrder.Size = new System.Drawing.Size(622, 117);
+            this.btnAddToOrder.TabIndex = 3;
+            this.btnAddToOrder.Text = "Add To Order";
+            this.btnAddToOrder.UseVisualStyleBackColor = true;
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.labelQuantityNumber);
+            this.panel1.Controls.Add(this.btnDecreaseQuantity);
+            this.panel1.Controls.Add(this.btnIncreaseQuantity);
+            this.panel1.Controls.Add(this.LabelCustomizationsLeft);
+            this.panel1.Controls.Add(this.LabelSizeOfDrinkLeft);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.btnLargeCupSize);
+            this.panel1.Controls.Add(this.btnMediumCupSize);
             this.panel1.Controls.Add(this.button1);
-            this.panel1.Controls.Add(this.button3);
-            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.btnIcedWater);
+            this.panel1.Controls.Add(this.btnCoffee);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnLatte);
+            this.panel1.Controls.Add(this.btnGreenLatte);
+            this.panel1.Controls.Add(this.btnIcedLatte);
             this.panel1.Location = new System.Drawing.Point(34, 35);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(622, 527);
             this.panel1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Latte";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(182, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Iced Latte";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(300, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(232, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Iced Matcha Green Tea Latte";
+            // 
+            // btnCoffee
+            // 
+            this.btnCoffee.Location = new System.Drawing.Point(26, 170);
+            this.btnCoffee.Name = "btnCoffee";
+            this.btnCoffee.Size = new System.Drawing.Size(112, 65);
+            this.btnCoffee.TabIndex = 6;
+            this.btnCoffee.Text = "Select";
+            this.btnCoffee.UseVisualStyleBackColor = true;
+            // 
+            // btnIcedWater
+            // 
+            this.btnIcedWater.Location = new System.Drawing.Point(169, 170);
+            this.btnIcedWater.Name = "btnIcedWater";
+            this.btnIcedWater.Size = new System.Drawing.Size(112, 65);
+            this.btnIcedWater.TabIndex = 8;
+            this.btnIcedWater.Text = "Select";
+            this.btnIcedWater.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(42, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Coffee";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(174, 142);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(96, 25);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Iced Water";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 290);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 65);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "S";
+            this.button1.UseVisualStyleBackColor = true;
+        
+            // 
+            // btnMediumCupSize
+            // 
+            this.btnMediumCupSize.Location = new System.Drawing.Point(26, 361);
+            this.btnMediumCupSize.Name = "btnMediumCupSize";
+            this.btnMediumCupSize.Size = new System.Drawing.Size(112, 65);
+            this.btnMediumCupSize.TabIndex = 13;
+            this.btnMediumCupSize.Text = "M";
+            this.btnMediumCupSize.UseVisualStyleBackColor = true;
+            // 
+            // btnLargeCupSize
+            // 
+            this.btnLargeCupSize.Location = new System.Drawing.Point(26, 432);
+            this.btnLargeCupSize.Name = "btnLargeCupSize";
+            this.btnLargeCupSize.Size = new System.Drawing.Size(112, 65);
+            this.btnLargeCupSize.TabIndex = 14;
+            this.btnLargeCupSize.Text = "L";
+            this.btnLargeCupSize.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(418, 290);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(164, 25);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Drink Here You Got";
+         
+            // LabelSizeOfDrinkLeft
+            // 
+            this.LabelSizeOfDrinkLeft.AutoSize = true;
+            this.LabelSizeOfDrinkLeft.Location = new System.Drawing.Point(470, 315);
+            this.LabelSizeOfDrinkLeft.Name = "LabelSizeOfDrinkLeft";
+            this.LabelSizeOfDrinkLeft.Size = new System.Drawing.Size(112, 25);
+            this.LabelSizeOfDrinkLeft.TabIndex = 16;
+            this.LabelSizeOfDrinkLeft.Text = "Size of Drink";
+            // 
+            // LabelCustomizationsLeft
+            // 
+            this.LabelCustomizationsLeft.AutoSize = true;
+            this.LabelCustomizationsLeft.Location = new System.Drawing.Point(448, 340);
+            this.LabelCustomizationsLeft.Name = "LabelCustomizationsLeft";
+            this.LabelCustomizationsLeft.Size = new System.Drawing.Size(134, 25);
+            this.LabelCustomizationsLeft.TabIndex = 17;
+            this.LabelCustomizationsLeft.Text = "Customizations";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label9);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Location = new System.Drawing.Point(714, 35);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(600, 527);
+            this.panel2.TabIndex = 5;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(33, 28);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(164, 25);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Drink Here You Got";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(108, 76);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(112, 25);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Size of Drink";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(108, 101);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(134, 25);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Customizations";
+            // 
+            // btnIncreaseQuantity
+            // 
+            this.btnIncreaseQuantity.Location = new System.Drawing.Point(253, 295);
+            this.btnIncreaseQuantity.Name = "btnIncreaseQuantity";
+            this.btnIncreaseQuantity.Size = new System.Drawing.Size(70, 65);
+            this.btnIncreaseQuantity.TabIndex = 18;
+            this.btnIncreaseQuantity.Text = "+";
+            this.btnIncreaseQuantity.UseVisualStyleBackColor = true;
+            // 
+            // btnDecreaseQuantity
+            // 
+            this.btnDecreaseQuantity.Location = new System.Drawing.Point(253, 380);
+            this.btnDecreaseQuantity.Name = "btnDecreaseQuantity";
+            this.btnDecreaseQuantity.Size = new System.Drawing.Size(70, 65);
+            this.btnDecreaseQuantity.TabIndex = 19;
+            this.btnDecreaseQuantity.Text = "-";
+            this.btnDecreaseQuantity.UseVisualStyleBackColor = true;
+            // 
+            // labelQuantityNumber
+            // 
+            this.labelQuantityNumber.AutoSize = true;
+            this.labelQuantityNumber.Location = new System.Drawing.Point(329, 290);
+            this.labelQuantityNumber.Name = "labelQuantityNumber";
+            this.labelQuantityNumber.Size = new System.Drawing.Size(90, 25);
+            this.labelQuantityNumber.TabIndex = 20;
+            this.labelQuantityNumber.Text = "(Quantity)";
+            // 
+            // btnPaymentOrderDrinkScreen
+            // 
+            this.btnPaymentOrderDrinkScreen.Location = new System.Drawing.Point(714, 556);
+            this.btnPaymentOrderDrinkScreen.Name = "btnPaymentOrderDrinkScreen";
+            this.btnPaymentOrderDrinkScreen.Size = new System.Drawing.Size(302, 117);
+            this.btnPaymentOrderDrinkScreen.TabIndex = 6;
+            this.btnPaymentOrderDrinkScreen.Text = "Proceed To Payment";
+            this.btnPaymentOrderDrinkScreen.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelOrderDrinkScreen
+            // 
+            this.btnCancelOrderDrinkScreen.Location = new System.Drawing.Point(1012, 556);
+            this.btnCancelOrderDrinkScreen.Name = "btnCancelOrderDrinkScreen";
+            this.btnCancelOrderDrinkScreen.Size = new System.Drawing.Size(302, 117);
+            this.btnCancelOrderDrinkScreen.TabIndex = 7;
+            this.btnCancelOrderDrinkScreen.Text = "Cancel";
+            this.btnCancelOrderDrinkScreen.UseVisualStyleBackColor = true;
             // 
             // OrderDrink
             // 
@@ -88,22 +328,50 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1366, 768);
-            this.Controls.Add(this.button4);
+            this.Controls.Add(this.btnCancelOrderDrinkScreen);
+            this.Controls.Add(this.btnPaymentOrderDrinkScreen);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.btnAddToOrder);
             this.Controls.Add(this.panel1);
             this.Margin = new System.Windows.Forms.Padding(13, 22, 13, 22);
             this.Name = "OrderDrink";
             this.Text = "OrderDrink";
             this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private Button button1;
-        private Button button2;
-        private Button button3;
-        private Button button4;
+        private Button btnLatte;
+        private Button btnIcedLatte;
+        private Button btnGreenLatte;
+        private Button btnAddToOrder;
         private Panel panel1;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private Label label4;
+        private Button btnIcedWater;
+        private Button btnCoffee;
+        private Label label6;
+        private Button btnLargeCupSize;
+        private Button btnMediumCupSize;
+        private Button button1;
+        private Label label5;
+        private Label labelQuantityNumber;
+        private Button btnDecreaseQuantity;
+        private Button btnIncreaseQuantity;
+        private Label LabelCustomizationsLeft;
+        private Label LabelSizeOfDrinkLeft;
+        private Panel panel2;
+        private Label label9;
+        private Label label8;
+        private Label label7;
+        private Button btnPaymentOrderDrinkScreen;
+        private Button btnCancelOrderDrinkScreen;
     }
 }
